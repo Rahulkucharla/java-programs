edaa37946938a5cdeaa1485abad044db3da3706a
Rahul kucharla <rahulkucharla20@gmail.com>
2021-05-25 16:20:20 +0530

    week1

--- week1/Week1A1.java
+++ week1/Week1A1.java
@@ -2,5 +2,7 @@ class Week1A1 {
 
   public static void main(String[] args) {
     // code...
+    // write here and commit push pull reqwait..let me try once.
+    // okay i'll disconnect now..wait
   }
 }
